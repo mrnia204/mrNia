15d8c9922dc66327409dee5a76df1276adfa7dfd
mrnia204 <204codetribe@gmail.com>
2025-10-07 10:20:50 +1000

    router fixt

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { HashRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import "remixicon/fonts/remixicon.css";
 
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <RouterProvider router={router} />
-  </HashRouter>
+     <RouterProvider router={router} />
 );
